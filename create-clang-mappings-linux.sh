0ddfb56bb6feaee2890c1278f17e6df4eaa3d96f
DarkCaster <dark.caster@outlook.com>
2019-04-16 02:37:17 +0300

    Added create-clang-mappings-linux.sh script: generate mappings for Linux x86 and x86_64 archs (some long types will differ)

---
+++ create-clang-mappings-linux.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#
+
+# generate mappings for linux version of clang library
+
+set -e
+
+script_dir="$( cd "$( dirname "$0" )" && pwd )"
+
+# copy clang includes
+clang_dir=`mktemp -d -t CLANG_INCLUDES_XXXXXXXXXXX`
+
+arch=`uname -m`
+
+cp -r /usr/include/clang-c "$clang_dir"
+
+build_generator() {
+  return 0
+}
+
+#--excludeFunctions
+
+generate_mappings() {
+  mono "$script_dir/ClangSharpPInvokeGenerator/bin/Debug/ClangSharpPInvokeGenerator.exe" \
+  --namespace ClangSharp_Linux_$arch \
+  --l libclang \
+  --file "$clang_dir/clang-c/Platform.h" \
+  --file "$clang_dir/clang-c/CXErrorCode.h" \
+  --file "$clang_dir/clang-c/CXString.h" \
+  --file "$clang_dir/clang-c/BuildSystem.h" \
+  --file "$clang_dir/clang-c/Documentation.h" \
+  --file "$clang_dir/clang-c/CXCompilationDatabase.h" \
+  --file "$clang_dir/clang-c/Index.h" \
+  --include "$clang_dir" \
+  --include "/usr/include" \
+  --output "$script_dir/ClangSharp/Generated_Linux_$arch.cs.tmp"
+
+  echo "#if LINUX_$arch" | cat - "$script_dir/ClangSharp/Generated_Linux_$arch.cs.tmp" > \
+  "$script_dir/ClangSharp/Generated_Linux_$arch.cs"
+  echo "#endif" >> "$script_dir/ClangSharp/Generated_Linux_$arch.cs"
+  rm "$script_dir/ClangSharp/Generated_Linux_$arch.cs.tmp"
+
+  sed -i "s| clang_| |g" "$script_dir/ClangSharp/Generated_Linux_$arch.cs"
+  sed -i "s|Methods|clang|g" "$script_dir/ClangSharp/Generated_Linux_$arch.cs"
+}
+
+build_generator
+generate_mappings
