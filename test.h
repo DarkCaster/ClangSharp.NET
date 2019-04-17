5d89056cb9112b19fa370c1e90a4985066e2883a
DarkCaster <dark.caster@outlook.com>
2019-04-18 02:04:43 +0300

    Added test.h header - simple test to detect how "long" types generated

---
+++ test.h
@@ -0,0 +1,12 @@
+//for c and c++ test
+//#include <stddef.h>
+//#include <stdint.h>
+
+//for c++ test
+#include <cstddef>
+#include <cstdint>
+
+typedef struct {
+  size_t sz_size_t;
+  uint64_t sz_u64;
+} non_anon;
